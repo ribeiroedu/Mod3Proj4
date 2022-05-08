4b9765924eb625905aa4f76fe08f3a8cc6c307ae
ribeiroedu <ed.almeidaribeiro@gmail.com>
2022-05-08 16:01:30 -0300

    cors add

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 3000;
 const app = express();
 const characters = require('./src/routes/characters.routes');
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
+
 app.use(express.json());
 
 const connectToDatabase = require('./src/database/database');
